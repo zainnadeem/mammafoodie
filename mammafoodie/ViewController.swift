8f59d1134c39766057f9ca6dffb7cfba8875b6af
zainnadeem <zn.nadeem@gmail.com>
2017-06-07 17:01:44 +0530

    Initial Commit

---
+++ mammafoodie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mammafoodie
+//
+//  Created by Zain Nadeem on 6/7/17.
+//  Copyright © 2017 Zain Nadeem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
