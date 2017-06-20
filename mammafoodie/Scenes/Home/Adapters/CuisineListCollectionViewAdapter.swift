a2ef62c78bdbbb755a793e426aa86a4bd94462b3
Akshit Zaveri <akshit@aakarsolutions.com>
2017-06-20 11:32:55 +0530

    Added KVO to contentOffset in UICollectionView. On change of the value adjust the selectionIndicatorView's leading constraint accordingly.

--- mammafoodie/Scenes/Home/Adapters/CuisineListCollectionViewAdapter.swift
+++ mammafoodie/Scenes/Home/Adapters/CuisineListCollectionViewAdapter.swift
@@ -7,6 +7,11 @@ class CuisineListCollectionViewAdapter: NSObject, UICollectionViewDelegate, UICo
     var selectionIndicatorView: UIView!
     var conLeadingViewSelectionIndicator: NSLayoutConstraint!
     
+    var selectedIndexPath: IndexPath = IndexPath(item: 0, section: 0)
+    
+    // Stored property for contentOffset observer
+    var contentOffsetKVOContext: UInt8 = 1
+    
     func createStaticData() {
         self.cuisines.append(MFCuisine(id: "1", name: "Italian", isSelected: true))
         self.cuisines.append(MFCuisine(id: "2", name: "Chinese", isSelected: false))
@@ -25,8 +30,9 @@ class CuisineListCollectionViewAdapter: NSObject, UICollectionViewDelegate, UICo
         
         let name: String = "MenuCuisineSelectorClnCell"
         self.collectionView.register(UINib(nibName: name, bundle: nil), forCellWithReuseIdentifier: name)
+        
+        self.addObserverForContentOffset()
     }
-    
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: MenuCuisineSelectorClnCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MenuCuisineSelectorClnCell", for: indexPath) as! MenuCuisineSelectorClnCell
@@ -39,10 +45,24 @@ class CuisineListCollectionViewAdapter: NSObject, UICollectionViewDelegate, UICo
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let theAttributes:UICollectionViewLayoutAttributes! = collectionView.layoutAttributesForItem(at: indexPath)
-        let cellFrameInSuperview:CGPoint = collectionView.convert(theAttributes.center, to: collectionView.superview)
-        self.conLeadingViewSelectionIndicator.constant = cellFrameInSuperview.x
-        
+        self.updateSelectionIndicatorView(in: collectionView, at: indexPath, animated: false)
+        self.updateSelection(for: indexPath)
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 60, height: collectionView.frame.height)
+    }
+    
+    func updateSelectionIndicatorView(in collectionView: UICollectionView, at indexPath: IndexPath, animated: Bool) {
+        let theAttributes: UICollectionViewLayoutAttributes! = collectionView.layoutAttributesForItem(at: indexPath)
+        let cellFrameInSuperview: CGPoint = collectionView.convert(theAttributes.center, to: collectionView.superview)
+        UIView.animate(withDuration: animated ? 0.27 : 0) {
+            self.conLeadingViewSelectionIndicator.constant = cellFrameInSuperview.x - collectionView.frame.origin.x
+        }
+    }
+    
+    func updateSelection(for indexPath: IndexPath) {
         for (index, var cuisine) in self.cuisines.enumerated() {
             if cuisine.isSelected {
                 cuisine.isSelected = false
@@ -51,9 +71,21 @@ class CuisineListCollectionViewAdapter: NSObject, UICollectionViewDelegate, UICo
         }
         self.cuisines[indexPath.item].isSelected = !self.cuisines[indexPath.item].isSelected
         collectionView.reloadData()
+        self.selectedIndexPath = indexPath
+    }
+}
+
+// Key-Value observer for contentOffset
+extension CuisineListCollectionViewAdapter {
+    
+    func addObserverForContentOffset() {
+        self.collectionView.addObserver(self, forKeyPath: "contentOffset", options: .new, context: &contentOffsetKVOContext)
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 60, height: collectionView.frame.height)
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if context == &contentOffsetKVOContext {
+            let collectionView: UICollectionView = object as! UICollectionView
+            self.updateSelectionIndicatorView(in: collectionView, at: self.selectedIndexPath, animated: false)
+        }
     }
 }
