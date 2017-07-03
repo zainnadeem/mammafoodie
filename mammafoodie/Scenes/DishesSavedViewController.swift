7942f77dbb254b09610f58ace9fea340a72f8cb1
Sireesha V <srivk1000@gmail.com>
2017-07-03 13:16:42 +0530

    Dishes Saved List

--- mammafoodie/Scenes/DishesSavedViewController.swift
+++ mammafoodie/Scenes/DishesSavedViewController.swift
@@ -1,19 +1,22 @@
-//
-//  DishesSavedViewController.swift
-//  mammafoodie
-//
-//  Created by Sireesha V on 7/3/17.
-//  Copyright © 2017 Zain Nadeem. All rights reserved.
-//
 
 import UIKit
 
-class DishesSavedViewController: UIViewController {
+class DishesSavedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var dishesListTableView: UITableView!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        dishesListTableView.delegate = self
+        dishesListTableView.dataSource = self
+        dishesListTableView.rowHeight = UITableViewAutomaticDimension
+        dishesListTableView.estimatedRowHeight = 270
+        
+        let name: String = "MenuItemTblCell"
+        dishesListTableView.register(UINib(nibName: name, bundle: nil), forCellReuseIdentifier: name)
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +24,29 @@ class DishesSavedViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: MenuItemTblCell = dishesListTableView.dequeueReusableCell(withIdentifier: "MenuItemTblCell", for: indexPath) as! MenuItemTblCell
+//            cell.setup(with: self.menu[indexPath.item])
+            cell.imgView.image = UIImage(named: "AlexaGrimes")!
+            cell.lblDishName.text = "Chiken Fries"
+            cell.lblUsername.text =  "Tim"
+            cell.imgViewProfilePicture.image = UIImage(named: "Bitmap")!
+            return cell
     }
-    */
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+            (cell as! MenuItemTblCell).cellWillDisplay()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 10
+    }
+    
+        
+   
+    
+
 
 }
